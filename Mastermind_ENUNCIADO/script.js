51e65b3fe8d5d360aeef4b4a9b1067982812702d
isildu <oriol.canellas.7e3@itb.cat>
2025-02-16 19:51:19 +0100

    rthtj n

--- Mastermind_ENUNCIADO/script.js
+++ Mastermind_ENUNCIADO/script.js
@@ -6,20 +6,27 @@ const GREY = "grey";
 const WHITE = "white";
 const BLACK = "black";
 
+const msj_sinIntentos = "No tienes más intentos :( "
+const msj_acertado = "Has acertado! Enhorabuena! :D "
+const msj_intento = "Intentos restantes: "
+
 
 //Declaración de variables globales.
 const master = [];
 const userCombi = [];
-var intento = 0;
+let intento = 0;
 var aciertos = 0;
 contador = 0;
 
 function init() {
+    document.querySelector("#info").textContent = msj_intento + MAX_INTENTOS;
     //1. Genera el código random del master
-    for(let i = 0; i < MAX_COMBI_COLORES; i++) {
-        let code = (COLORS[Math.floor(Math.random() * COLORS.length)]);
-        master[i] = code;
-      }
+    while (master.length < MAX_COMBI_COLORES) {
+        let code = COLORS[Math.floor(Math.random() * COLORS.length)];
+        if (!master.includes(code)) {
+            master.push(code);
+        }
+    }
       console.log(master);
     //2. Crea todas las filas según el número de intentos.
     for(let j = 0; j < MAX_INTENTOS; j ++){
@@ -34,34 +41,79 @@ function init() {
 introducido el usuario.
 Informamos al usuario del resultado y del número de intentos que lleva*/
 function Comprobar() {
-    contador = 0;
+    if(userCombi.length != 4){
+        alert ("afegeix 4 colors crac")
+        añadeColor(color)
+    }
     document.getElementById("combiText").value = "";
 
     arrayFilas=document.getElementsByClassName("rowResult");
-    for(let numFila=0;numFila<arrayFilas.length; numFila ++){
-        const celes = arrayFilas[numFila].getElementsByClassName('celUserCombi flex');
+        const celes = arrayFilas[intento].getElementsByClassName('celUserCombi flex');
         for(let j = 0; j < MAX_COMBI_COLORES; j++){
-            celes[j].style.backgroundColor = combo2[j];
+            celes[j].style.backgroundColor = userCombi[j];
         }
-    }
+        if(intento + 1 >= MAX_INTENTOS) {
+            document.querySelector("#info").textContent = msj_sinIntentos;
+            revelarCombinacion();
+            return
+        }
+    
+        if (comprobarCombinacion()){
+            document.querySelector("#info").textContent = msj_acertado;
+        }else{
+            intento++;
+            contador = 0;
+            document.querySelector("#info").textContent = msj_intento + (MAX_INTENTOS - intento);
+        }    
     //const filas = document.getElementsByClassName('celUserCombi flex');
   
     //intento ++
 }
+function revelarCombinacion() {
+    for (let color in master){
+     let mostrarMaster= document.querySelectorAll("#master .w100 .w25");
+      mostrarMaster[color].querySelector(".cel").style.background = master[color];
+    }
+}
 
 /** Procedimiento que se ejecuta cada vez que el usuario selecciona un color, hasta el número máximo de colores permitidos en la combinación. */
-let combo2 = [];
 function añadeColor(color) {
-    console.log("color: ",color, combo2)
+    console.log("color: ",color, userCombi)
     let inFilas = document.getElementById("combiText");
     if(contador < MAX_COMBI_COLORES){
         inFilas.value += " " + color;
-        combo2[contador] = color;
+        userCombi[contador] = color;
         contador ++;
     }else alert("maximo de numeros añadidos")
     
 }
 
+function comprobarCombinacion() {
+    let aciertos = 0;
+
+    for (let color in userCombi) {
+        let posColor = master.indexOf(userCombi[color])
+        if(posColor != -1){
+            //el color existe
+            if(color == posColor){
+               circuloColor(color).style.background = BLACK
+               aciertos++;
+            }else{
+                circuloColor(color).style.background = WHITE
+            }
+        }else{
+            //el color no existe
+            circuloColor(color).style.background = GREY
+        }
+    }
+    return aciertos == 4;
+}
+function circuloColor(color){
+    let rowResults = document.querySelectorAll("#Result div.rowResult");
+    let rowIntento = rowResults[intento];
+    let celda = rowIntento.querySelectorAll(".rowCercleResult div.w40");
+    return celda[color].querySelector('.cercleResult')
+}
 
 /** Template con el código HTML que corresponde a cada fila de juego/intento. */
 const ROW_RESULT = `<div class="rowResult w100 flex wrap">
